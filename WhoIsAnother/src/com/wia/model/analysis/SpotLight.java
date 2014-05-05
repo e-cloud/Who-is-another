9795228b70fde3904dbd661fe20b3f30f392aa57
Scott <saintscott119@gmail.com>
2014-05-05 09:19:11 +0800

    revert change

--- WhoIsAnother/src/com/wia/model/analysis/SpotLight.java
+++ WhoIsAnother/src/com/wia/model/analysis/SpotLight.java
@@ -41,7 +41,7 @@ public class SpotLight {
 		Context context = Context.getInstance();
 		this.author = context.getCurrentAuthor();
 		this.ACInFirstSubmit = 0;
-		//init();
+		init();
 	}
 
 	/**
@@ -73,61 +73,47 @@ public class SpotLight {
 		Iterator<Problem> problemIterator = problems.iterator();
 		while (problemIterator.hasNext()) {
 			Problem p = problemIterator.next();
-			Iterator<SubmitLog> submitIterator = p.getSubmitMap().values().iterator();
-			//Iterator<SubmitLog> compare = p.getSubmitMap().values().iterator();
-			//SubmitLog submitlog = compare.next();
-			//List<SubmitLog> list;
+			Iterator<SubmitLog> submitIterator = p.getSubmitMap().values()
+					.iterator();
 			List<SubmitLog> list = new ArrayList<SubmitLog>();
 			Iterator<SubmitLog> log = p.getSubmitMap().values().iterator();
-			while(log.hasNext()){
+			while (log.hasNext()) {
 				SubmitLog l = log.next();
 				list.add(l);
 			}
-			/*Collections.sort(list,new Comparator<SubmitLog>() {
-				@Override
-				public int compare(SubmitLog p1,SubmitLog p2) {
-					if (p1.getRid() > p2.getRid())
-						return 0;
-					else
-						return 1;
-				}
-			});*/
 			int min = list.get(0).getRid();
-			for(int i=0;i<list.size();i++){
-				if(list.get(i).getRid()<min)
+			for (int i = 0; i < list.size(); i++) {
+				if (list.get(i).getRid() < min)
 					min = list.get(i).getRid();
-				}
-			for(int i=0;i<list.size();i++){
-				if(list.get(i).getRid() == min)
-					if(list.get(i).getStatus().toString() == "AC")
-				//System.out.println(list.get(0).getPid());
+			}
+			for (int i = 0; i < list.size(); i++) {
+				if (list.get(i).getRid() == min)
+					if (list.get(i).getStatus().toString() == "AC")
 						ACInFirstSubmit++;
 			}
 			while (submitIterator.hasNext()) {
 				SubmitLog s = submitIterator.next();
 				if (s.getStatus().toString() == "AC") {
-					acceptedTimeInterval[s.getSubmitTime().get(Calendar.HOUR_OF_DAY)-1]++;
-				} else
-					unsolvedTimeInterval[s.getSubmitTime().get(Calendar.HOUR_OF_DAY)-1]++;
-				//if (s.getSubmitTime().before(submitlog.getSubmitTime()))
-					//compare = submitIterator;
+					int index = s.getSubmitTime().get(Calendar.HOUR_OF_DAY);
+					acceptedTimeInterval[index]++;
+				} else {
+					int index = s.getSubmitTime().get(Calendar.HOUR_OF_DAY);
+					unsolvedTimeInterval[index]++;
+				}
 			}
-			//if(compare.next().getStatus().toString() == "AC")
-			
 		}
 		for (int i = 0; i < 24; i++) {
 			submitTimeInterval[i] = acceptedTimeInterval[i]
 					+ unsolvedTimeInterval[i];
 		}
-		System.out.print("1");
 	}
 
 	/**
 	 * @return 解决问题数最多的10天
 	 */
 	public List<Pair<String, Integer>> problemSolvedTop10() {
-	//public List<Integer> problemSolvedTop10() {
-		//new SimpleDateFormat().format(new Date());
+		// public List<Integer> problemSolvedTop10() {
+		// new SimpleDateFormat().format(new Date());
 		SimpleDateFormat stdf = new SimpleDateFormat("yyyy-MM-dd");
 		List<Pair<Date, Integer>> tempProblemSolvedTop10;
 		List<Pair<String, Integer>> getProblemSolvedTop10 = new ArrayList<Pair<String, Integer>>();
@@ -148,7 +134,8 @@ public class SpotLight {
 		// 取出前十天
 		for (int i = 0; i < tempProblemSolvedTop10.size(); i++) {
 			Pair<Date, Integer> p = tempProblemSolvedTop10.get(i);
-			Pair<String,Integer> p1 = new Pair<String,Integer>(stdf.format(p.getKey()),p.getValue());
+			Pair<String, Integer> p1 = new Pair<String, Integer>(stdf.format(p
+					.getKey()), p.getValue());
 			getProblemSolvedTop10.add(p1);
 		}
 		return getProblemSolvedTop10;
